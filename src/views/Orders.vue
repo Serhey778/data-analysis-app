1adf00dff5b8b2cbee4cd27071c0afa7af05f08b
Serhey 778 <muha_778sergey@mail.ru>
2025-10-14 14:19:08 +0300

    create api response

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue';
+
+const ordersData = ref(null);
+const url =
+  'http://109.73.206.144:6969/api/orders?dateFrom=2025-10-01&dateTo=2025-10-10&page=1&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie&limit=2';
+async function fetchOrdersData() {
+  ordersData.value = null;
+  const res = await fetch(url);
+  ordersData.value = await res.json();
+  console.log(ordersData.value.data);
+  console.log(ordersData.value.data[0]);
+}
+fetchOrdersData();
+</script>
 
 <template>
   <header>Orders!!!</header>
